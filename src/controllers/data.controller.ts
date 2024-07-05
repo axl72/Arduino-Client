4b5fe0deea81e78242989e41c78f8dd7598c0836
axl72 <axell.bernabel@unmsm.edu.pe>
2024-07-05 09:29:03 -0500

    updated data json

--- src/controllers/data.controller.ts
+++ src/controllers/data.controller.ts
@@ -2,10 +2,24 @@ import { Response, Request } from "express"
 
 export class DataController {
     getDataFromArduino = ( req: Request, res: Response ) => {
+        function getRandomNumber() {
+            return Math.floor(Math.random() * enterprises.length);
+        }
+
+        const enterprises = [
+            'AUSTRAL GROUP S.A.A',
+            'ANDINA DE DESARROLLO ANDESA S.A.C.',
+            'ANDINA DE DESARROLLO ANDESA S.A.C.',
+            'INDUSTRIA PESQUERA SANTA MONICA S.A.',
+            'DEXIM S.R.L',
+            'PESQUERA CANTABRIA S.A',
+
+        ]
+        const number = Number(Math.random() * 1000 ).toFixed(2)
         const data =  {
-           data1: Math.random() * 300,
-           data2: Math.random() * 100,
-           data3: Math.random() * Math.PI
+           enterprise: enterprises[getRandomNumber()],
+           kilograms: number,
+           fishingArea: 'PACIFIC OCEAN'
         }
 
         res.status(200).json(data)
